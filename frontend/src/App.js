c490404ad67b889ea27db7736a6f77d9ef74804f
Holda11 <janholda@seznam.cz>
2022-11-20 13:05:17 +0100

    Přidání výčetu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./admin/pages/Dashboard";
 import Login from "./admin/pages/loginPage";
 import NewNews from "./admin/pages/newNews";
+import NovinkyDash from "./admin/pages/NovinkyDash";
 import './App.css';
 import Home from './pages';
 import Novinky from "./pages/Novinky";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/NewNews" exact element={<NewNews />}/>
         <Route path="/Login" exact element={<Login />}/>
         <Route path="/Dashboard" exact element={<Dashboard />}/>
+        <Route path="/NovinkyDashboard" exact element={<NovinkyDash />}/>
       </Routes>
       </BrowserRouter>
   );
---
+++ frontend/src/admin/pages/NovinkyDash.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Vycet from '../components/News/Vycet'
+
+const NovinkyDash = () => {
+  return (
+    <div>
+        <Vycet/>
+    </div>
+  )
+}
+
+export default NovinkyDash
